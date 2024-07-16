03312a68b6b08a9c95869b3485e2b43967159831
Yassine Bridi <ybridi@gmail.com>
2024-07-16 23:51:15 +0100

    (feat): fix ast_grep

--- src/redux/thunk/process_line_replace.rs
+++ src/redux/thunk/process_line_replace.rs
@@ -1,16 +1,18 @@
-use std::{fs, sync::Arc};
+use std::{fs, process::Command, sync::Arc};
 
 use async_trait::async_trait;
 use redux_rs::{middlewares::thunk::Thunk, StoreApi};
 use regex::RegexBuilder;
+use serde_json::from_str;
 use tokio::sync::mpsc::UnboundedSender;
 
 use crate::{
   action::{AppAction, TuiAction},
+  astgrep::AstGrepOutput,
   components::notifications::NotificationEnum,
   redux::{
     action::Action,
-    state::{ReplaceTextKind, SearchTextKind, State},
+    state::{Match, ReplaceTextKind, ReplaceTextState, SearchTextKind, SearchTextState, State},
     thunk::ThunkAction,
   },
 };
@@ -34,73 +36,122 @@ impl ProcessLineReplaceThunk {
     if let Some(search_result) = search_list.list.get(self.file_index) {
       if let Some(match_info) = search_result.matches.get(self.line_index) {
         let file_path = &search_result.path;
-        let content = fs::read_to_string(file_path).expect("Unable to read file");
-        let mut lines: Vec<String> = content.lines().map(String::from).collect();
 
-        let re = match search_text_state.kind {
-          SearchTextKind::Regex => {
-            RegexBuilder::new(&search_text_state.text).case_insensitive(true).build().expect("Invalid regex")
-          },
-          SearchTextKind::MatchCase => {
-            RegexBuilder::new(&regex::escape(&search_text_state.text))
-              .case_insensitive(false)
-              .build()
-              .expect("Invalid regex")
-          },
-          SearchTextKind::MatchWholeWord => {
-            RegexBuilder::new(&format!(r"\b{}\b", regex::escape(&search_text_state.text)))
-              .case_insensitive(true)
-              .build()
-              .expect("Invalid regex")
-          },
-          SearchTextKind::MatchCaseWholeWord => {
-            RegexBuilder::new(&format!(r"\b{}\b", regex::escape(&search_text_state.text)))
-              .case_insensitive(false)
-              .build()
-              .expect("Invalid regex")
-          },
-          SearchTextKind::Simple => {
-            RegexBuilder::new(&regex::escape(&search_text_state.text))
-              .case_insensitive(true)
-              .build()
-              .expect("Invalid regex")
-          },
-          #[cfg(feature = "ast_grep")]
-          SearchTextKind::AstGrep => panic!("AST-grep is not supported for line-by-line replacement"),
-        };
-
-        if let Some(line) = lines.get_mut(match_info.line_number - 1) {
-          let replaced_line = re.replace_all(line, |caps: &regex::Captures| {
-            let matched_text = caps.get(0).unwrap().as_str();
-            match replace_text_state.kind {
-              ReplaceTextKind::PreserveCase => {
-                let first_char = matched_text.chars().next().unwrap_or_default();
-                if matched_text.chars().all(char::is_uppercase) {
-                  replace_text_state.text.to_uppercase()
-                } else if first_char.is_uppercase() {
-                  replace_text_state
-                    .text
-                    .chars()
-                    .enumerate()
-                    .map(|(i, rc)| if i == 0 { rc.to_uppercase().to_string() } else { rc.to_lowercase().to_string() })
-                    .collect::<String>()
-                } else {
-                  replace_text_state.text.to_lowercase()
-                }
-              },
-              ReplaceTextKind::Simple => replace_text_state.text.to_string(),
-              #[cfg(feature = "ast_grep")]
-              ReplaceTextKind::AstGrep => panic!("AST-grep is not supported for line-by-line replacement"),
-            }
-          });
-          *line = replaced_line.into_owned();
+        #[cfg(feature = "ast_grep")]
+        if search_text_state.kind == SearchTextKind::AstGrep {
+          self
+            .process_ast_grep_replace(
+              file_path,
+              &search_text_state.text,
+              &replace_text_state.text,
+              match_info.line_number,
+            )
+            .await;
+        } else {
+          process_normal_replace(search_text_state, match_info, replace_text_state, file_path);
         }
 
-        let new_content = lines.join("\n");
-        fs::write(file_path, new_content).expect("Unable to write file");
+        #[cfg(not(feature = "ast_grep"))]
+        process_normal_replace(search_text_state, match_info, replace_text_state, file_path);
       }
     }
   }
+
+  async fn process_ast_grep_replace(
+    &self,
+    file_path: &str,
+    search_pattern: &str,
+    replace_pattern: &str,
+    line_number: usize,
+  ) {
+    let output = Command::new("ast-grep")
+      .args(["run", "-p", search_pattern, "-r", replace_pattern, "--json=compact", file_path])
+      .output()
+      .expect("Failed to execute ast-grep for replacement");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let ast_grep_results: Vec<AstGrepOutput> = from_str(&stdout).expect("Failed to parse ast-grep output");
+
+    let mut content = fs::read_to_string(file_path).expect("Unable to read file");
+
+    for result in ast_grep_results.iter().rev() {
+      if result.range.start.line == line_number {
+        if let (Some(replacement), Some(offsets)) = (&result.replacement, &result.replacement_offsets) {
+          let start = offsets.start;
+          let end = offsets.end;
+          content.replace_range(start..end, replacement);
+        }
+      }
+    }
+
+    fs::write(file_path, content).expect("Unable to write file");
+  }
+}
+
+fn process_normal_replace(
+  search_text_state: SearchTextState,
+  match_info: &Match,
+  replace_text_state: ReplaceTextState,
+  file_path: &str,
+) {
+  let content = fs::read_to_string(file_path).expect("Unable to read file");
+  let mut lines: Vec<String> = content.lines().map(String::from).collect();
+
+  let re = match search_text_state.kind {
+    SearchTextKind::Regex => {
+      RegexBuilder::new(&search_text_state.text).case_insensitive(true).build().expect("Invalid regex")
+    },
+    SearchTextKind::MatchCase => {
+      RegexBuilder::new(&regex::escape(&search_text_state.text)).case_insensitive(false).build().expect("Invalid regex")
+    },
+    SearchTextKind::MatchWholeWord => {
+      RegexBuilder::new(&format!(r"\\b{}\\b", regex::escape(&search_text_state.text)))
+        .case_insensitive(true)
+        .build()
+        .expect("Invalid regex")
+    },
+    SearchTextKind::MatchCaseWholeWord => {
+      RegexBuilder::new(&format!(r"\\b{}\\b", regex::escape(&search_text_state.text)))
+        .case_insensitive(false)
+        .build()
+        .expect("Invalid regex")
+    },
+    SearchTextKind::Simple => {
+      RegexBuilder::new(&regex::escape(&search_text_state.text)).case_insensitive(true).build().expect("Invalid regex")
+    },
+    #[cfg(feature = "ast_grep")]
+    SearchTextKind::AstGrep => unreachable!(),
+  };
+
+  if let Some(line) = lines.get_mut(match_info.line_number - 1) {
+    let replaced_line = re.replace_all(line, |caps: &regex::Captures| {
+      let matched_text = caps.get(0).unwrap().as_str();
+      match replace_text_state.kind {
+        ReplaceTextKind::PreserveCase => {
+          let first_char = matched_text.chars().next().unwrap_or_default();
+          if matched_text.chars().all(char::is_uppercase) {
+            replace_text_state.text.to_uppercase()
+          } else if first_char.is_uppercase() {
+            replace_text_state
+              .text
+              .chars()
+              .enumerate()
+              .map(|(i, rc)| if i == 0 { rc.to_uppercase().to_string() } else { rc.to_lowercase().to_string() })
+              .collect::<String>()
+          } else {
+            replace_text_state.text.to_lowercase()
+          }
+        },
+        ReplaceTextKind::Simple => replace_text_state.text.to_string(),
+        #[cfg(feature = "ast_grep")]
+        ReplaceTextKind::AstGrep => unreachable!(),
+      }
+    });
+    *line = replaced_line.into_owned();
+  }
+
+  let new_content = lines.join("\n");
+  fs::write(file_path, new_content).expect("Unable to write file");
 }
 
 #[async_trait]
